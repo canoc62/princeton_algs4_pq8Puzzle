1bb39ef020c3360789a91bebe1aae5729d22f9c9
Christopher Cano <canoc4262@gmail.com>
2016-03-03 16:32:08 -0800

    Create board and solver skeletons.

---
+++ Board.java
@@ -0,0 +1,49 @@
+
+
+public class Board {
+    public Board(int[][] blocks) {
+        
+    }
+    
+    public int dimensions() {
+        
+    }
+    
+    public int hamming() {
+        
+    }
+    
+    public int manhattan() {
+        
+    }
+    
+    public boolean isGoal() {
+        
+    }
+    
+    public Board twin() {
+        
+    }
+    
+    public boolean equals(Object y) {
+        
+    }
+    
+    public Iterable<Board> neighbors() {
+        
+    }
+    
+    public String toString() {
+        
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+                                        
+                                      
+    
+}
---
+++ Solver.java
@@ -0,0 +1,26 @@
+
+public class Solver {
+    public Solver(Board initial) {
+    
+    }
+    
+    public boolean isSolvable() {
+        
+    }
+    
+    public int moves() {
+        
+    }
+    
+    public Iterable<Board> solution() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+                                  
+                                  
+
+    
+}
