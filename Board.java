a76a3088d504bb5edb0a799ac1bb4bbdcebbc7eb
Christopher Cano <canoc4262@gmail.com>
2016-03-05 16:45:19 -0800

    Fix heuristic functions to avoid checking for 0 tile.

--- Board.java
+++ Board.java
@@ -4,21 +4,21 @@ import edu.princeton.cs.algs4.MinPQ;
 public class Board {
     
     private int[][] board;
-    private int dimensionSize;
+    private int N;
     private int blockNumber;
     private int hammingCount;
     private int manhattanCount;
     
     private int tileNumber(int i, int j) {
-        return (i*dimensionSize) + j;
+        return (i*N) + j;
     }
     
     private int computeManhattanI(int tileNum){
-        return tileNum/dimensionSize;
+        return tileNum/N;
     }
     
     private int computeManhattanJ(int tileNum) {
-        int manhattanJ = tileNum - (computeManhattanI(tileNum)*dimensionSize);
+        int manhattanJ = tileNum - (computeManhattanI(tileNum)*N);
         return manhattanJ; 
     }
     
@@ -30,18 +30,18 @@ public class Board {
     
     public Board(int[][] blocks) {
         board = blocks; 
-        dimensionSize = blocks[1].length;
+        N = blocks[1].length;
     }
     
-    public int dimensions() {
-        return dimensionSize;
+    public int dimension() {
+        return N;
     }
     
     public int hamming() {
         hammingCount = 0;
         
-        for (int i = 0; i < dimensionSize; i++) {
-            for (int j = 0; j < dimensionSize; j++) {
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
                 if(board[i][j] != tileNumber(i,j)) {
                     hammingCount++;
                 }
@@ -55,8 +55,9 @@ public class Board {
         
         for (int i = 0; i < dimensionSize; i++) {
             for (int j = 0; j < dimensionSize; j++) {
-                if(board[i][j] != tileNumber(i,j)) {
-                    manhattanCount += computeManhattanCount(board[i][j],i,j);
+                int tile = board[i][j];
+                if(tile != 0 && tile != tileNumber(i,j)) {
+                    manhattanCount += computeManhattanCount(tile,i,j);
                 }
             }
         }
@@ -64,7 +65,14 @@ public class Board {
     }
     
     public boolean isGoal() {
-        
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                if(board[i][j] != tileNumber(i,j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     
     public Board twin() {
